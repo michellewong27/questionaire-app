2ae4c7d19f593148df7ede02b42209969b1a464f
Michelle Wong <michellekwong2@gmail.com>
2020-09-25 17:16:10 -0400

    Added in quiz data

---
+++ script.js
@@ -0,0 +1,50 @@
+const quizData = [
+  {
+    question: 'What year was React released?',
+    a: '2005',
+    b: '2009',
+    c: '2016',
+    d: '2013',
+    correct: 'd'
+  },
+  {
+    question: 'How old is Michelle?',
+    a: '28',
+    b: '23',
+    c: '26',
+    d: '24',
+    correct: 'b'
+  }, 
+  {
+    question: 'What is the most used programming language in 2019?',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'JavaScript',
+    corect: 'd'
+  },
+  {
+    question: 'Who was the president in 1982?',
+    a: 'Jimmy Carter',
+    b: 'Ronald Reagan',
+    c: 'Richard Nixon',
+    d: 'George H. W. Bush',
+    correct: 'b'
+  },
+  {
+    question: 'What does HTML stand for?',
+    a: 'Hypertext Markup Language',
+    b: 'Hyperactive Text Monitoring Language',
+    c: 'History Text Memo Language',
+    d: 'Hack Title Micro Lynx',
+    correct: 'a'
+  }, 
+  {
+    question: 'What year was JavaScript launched?',
+    a: '1995',
+    b: '1994',
+    c: '1996',
+    d: 'None of the above',
+    correct: 'd'
+  }
+]
