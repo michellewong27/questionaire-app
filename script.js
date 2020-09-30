05b28b7b42d0a48ba4999015f5c15007e0926921
Michelle Wong <michellekwong2@gmail.com>
2020-09-29 20:16:51 -0400

    Event listener on submit btn

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit')
 
 let currentQuiz = 0;
 
@@ -68,5 +69,11 @@ function loadQuiz(){
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
 
-  currentQuestion++;
+  currentQuiz++;
 }
+
+submitBtn.addEventListener('click', () => {
+  currentQuiz++;
+  loadQuiz();
+
+})
